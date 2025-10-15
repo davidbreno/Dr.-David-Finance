208241c8816620717e85dc0d9a0c24a77701e0ec
davidbreno <david.breno2013@hotmail.com>
2025-10-15 14:29:36 -0300

    debug: wrap AppShell parts with labeled error boundaries (finance-david)

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -4,18 +4,24 @@ import type { PropsWithChildren } from "react";
 import { Sidebar } from "./Sidebar";
 import { Topbar } from "./Topbar";
 
+import { LabeledErrorBoundary } from "../common/ErrorBoundary";
+
+import { LabeledErrorBoundary } from "../common/ErrorBoundary";
+
 export const AppShell = ({ children }: PropsWithChildren) => {
   return (
     <div className="flex min-h-screen flex-col bg-[var(--color-bg)] lg:flex-row">
-      <Sidebar />
+      <LabeledErrorBoundary label="Sidebar"><Sidebar \/><\/LabeledErrorBoundary>
       <div className="flex flex-1 flex-col gap-6 px-4 pb-16 pt-4 sm:px-6 lg:px-8 lg:pb-8">
-        <Topbar />
+        <LabeledErrorBoundary label="Topbar"><Topbar \/><\/LabeledErrorBoundary>
         <main className="flex-1 overflow-y-auto">
           <div className="mx-auto flex w-full max-w-6xl flex-col gap-6 pb-12">
-            {children ?? <Outlet />}
+            <LabeledErrorBoundary label="MainChildren">{children ?? <Outlet \/>}<\/LabeledErrorBoundary>
           </div>
         </main>
       </div>
     </div>
   );
 };
+
+
